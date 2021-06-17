a0de918eab40a4f0caec6dfd50b70a9817b13fd9
Huy Truong <huy.truong@mevry.com>
2021-06-18 00:13:53 +0700

    run with failed test

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,6 @@ describe('Simple Math Test', () => {
         assert.strictEqual(1 + 1, 2);
     });
  it('should return 9', () => {
-        assert.strictEqual(3 * 3, 9);
+        assert.strictEqual(3 * 3, 8);
     });
 });
